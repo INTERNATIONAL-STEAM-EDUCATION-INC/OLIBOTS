6f2b1dfeb91a70a0a247caa38215d78c76efdf50
Pasasante01 <145374714+Pasasante01@users.noreply.github.com>
2023-10-28 16:37:26 -0500

    Update DeteccionDeColoresRojoVerde.py
    Colores

--- DeteccionDeColoresRojoVerde.py
+++ DeteccionDeColoresRojoVerde.py
@@ -4,42 +4,44 @@
 
 import numpy as np
 import cv2
-
+import mraa
+import time
 
 # Capturing video through webcam
 webcam = cv2.VideoCapture(0)
 
+gpio_1 = mraa.Gpio(23)
+gpio_2 = mraa.Gpio(24)
+
+gpio_1.dir(mraa.DIR_OUT)
+gpio_2.dir(mraa.DIR_OUT)
+time_ir = time.time()
+time_ig = time.time()
 # Start a while loop
 while(1):
     
     # Reading the video from the
     # webcam in image frames
-    _, imageFrame = webcam.read()
+    _, frame = webcam.read()
 
     # Convert the imageFrame in
     # BGR(RGB color space) to
     # HSV(hue-saturation-value)
     # color space
+    imageFrame = cv2.medianBlur(frame,25)
     hsvFrame = cv2.cvtColor(imageFrame, cv2.COLOR_BGR2HSV)
 
     # Set range for red color and
     # define mask
-    red_lower = np.array([136, 87, 111], np.uint8)
+    red_lower = np.array([140, 40, 40], np.uint8)
     red_upper = np.array([180, 255, 255], np.uint8)
     red_mask = cv2.inRange(hsvFrame, red_lower, red_upper)
-
-    # Set range for green color and
+     # Set range for green color and
     # define mask
-    green_lower = np.array([25, 52, 72], np.uint8)
-    green_upper = np.array([102, 255, 255], np.uint8)
+    green_lower = np.array([70, 60, 60], np.uint8)
+    green_upper = np.array([100, 255, 255], np.uint8)
     green_mask = cv2.inRange(hsvFrame, green_lower, green_upper)
 
-    # Set range for blue color and
-    # define mask
-    #blue_lower = np.array([94, 80, 2], np.uint8)
-    #blue_upper = np.array([120, 255, 255], np.uint8)
-    #blue_mask = cv2.inRange(hsvFrame, blue_lower, blue_upper)
-    
     # Morphological Transform, Dilation
     # for each color and bitwise_and operator
     # between imageFrame and mask determines
@@ -53,46 +55,58 @@ while(1):
     # For green color
     green_mask = cv2.dilate(green_mask, kernel)
     res_green = cv2.bitwise_and(imageFrame, imageFrame, mask = green_mask)
-    
-    # For blue color
-    #blue_mask = cv2.dilate(blue_mask, kernel)
-    #res_blue = cv2.bitwise_and(imageFrame, imageFrame, mask = blue_mask)
 
     # Creating contour to track red color
     contours, hierarchy = cv2.findContours(red_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    
-    for pic, contour in enumerate(contours):
-        area = cv2.contourArea(contour)
-        if(area > 300):
-            x, y, w, h = cv2.boundingRect(contour)
-            imageFrame = cv2.rectangle(imageFrame, (x, y), (x + w, y + h), (0, 0, 255), 2)
-            
-            cv2.putText(imageFrame, "Red Colour", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 255))    
-
+    if not contours:
+        time_fr = time.time()
+        time_tr = time_fr - time_ir
+        if time_tr >= 1:
+            gpio_2.write(0)
+    else:
+        for pic, contour in enumerate(contours):
+            area = cv2.contourArea(contour)
+            if(area > 30000):
+                x, y, w, h = cv2.boundingRect(contour)
+                imageFrame = cv2.rectangle(imageFrame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+                cv2.putText(imageFrame, "Red Colour", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 255))    
+                gpio_2.write(1)
+                time_ir = time.time()
+            else:
+                time_fr = time.time()
+                time_tr = time_fr - time_ir
+                if time_tr >= 1:
+                    gpio_2.write(0)
     # Creating contour to track green color
     contours, hierarchy = cv2.findContours(green_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    
-    for pic, contour in enumerate(contours):
-        area = cv2.contourArea(contour)
-        if(area > 300):
-            x, y, w, h = cv2.boundingRect(contour)
-            imageFrame = cv2.rectangle(imageFrame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            
-            cv2.putText(imageFrame, "Green Colour", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 255, 0))
+    if not contours:
+        time_fg = time.time()
+        time_tg = time_fg - time_ig
+        if time_tg >= 1:
+            gpio_1.write(0)
+    else:
+        for pic, contour in enumerate(contours):
+            area = cv2.contourArea(contour)
+            if(area > 30000):
+                x, y, w, h = cv2.boundingRect(contour)
+                imageFrame = cv2.rectangle(imageFrame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                
+                cv2.putText(imageFrame, "Green Colour", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 255, 0))
+                gpio_1.write(1)
+            else:
+                time_fg = time.time()
+                time_tg = time_fr - time_ir
+                if time_tg >= 1:
+                    gpio_1.write(0)
 
-    # Creating contour to track blue color
-    #contours, hierarchy = cv2.findContours(blue_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    #for pic, contour in enumerate(contours):
-        #area = cv2.contourArea(contour)
-        #if(area > 300):
-            #x, y, w, h = cv2.boundingRect(contour)
-            #imageFrame = cv2.rectangle(imageFrame, (x, y), (x + w, y + h), (255, 0, 0), 2)
-            
-            #cv2.putText(imageFrame, "Blue Colour", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 0, 0))
-            
     # Program Termination
     cv2.imshow("Multiple Color Detection in Real-TIme", imageFrame)
+    
+    
     if cv2.waitKey(10) & 0xFF == ord('q'):
+        cap.release()
+        cv2.destroyAllWindows()
+        break
         cap.release()
         cv2.destroyAllWindows()
         break
